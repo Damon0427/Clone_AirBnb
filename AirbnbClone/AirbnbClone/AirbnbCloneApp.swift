f74b3579be189cdabaa41f6d615fa1068305d213
Zicheng Tan <90937647+Damon0427@users.noreply.github.com>
2025-05-06 14:40:08 -0700

    Project Set Up

---
+++ AirbnbClone/AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Zicheng Tan on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
